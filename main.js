bcbbfe7fa56aa5918163514db22e313083a45b08
Matthew Glazar <MatthewGlazar01@gmail.com>
2023-04-07 14:50:53 -0400

    Marker Automation Attempt

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let map;
 
 async function initMap() {
-  // The location of Uluru
-  const position = { lat: 41.013889, lng: 28.955556 };
-  const position2 = { lat: 39.654722, lng: 66.975833 };
+// The location of Uluru
+  //const position = { lat: 41.013889, lng: 28.955556 };
+  //const position2 = { lat: 39.654722, lng: 66.975833 };
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
@@ -26,31 +26,34 @@ async function initMap() {
 
   mapContainer.appendChild(div);
 
-  new google.maps.Marker({
-  position: position,
-  map,
-  title: "City Name",
-  icon: image,
-  }).addListener("click", () => {
-	map.setCenter(position);
-	map.setZoom(6);
-	$("#cityInfo").load("./cityData/test.html");
-	let pop = document.getElementById("cityInfo");
-	pop.style.display ='block';
+
+  const cities = [
+    "./cityData/Chang'an.html",
+    "./cityData/Samarkand.html",
+  ];
+
+  cities.forEach(city => {
+    $("#cityInfo").load("./cityData/test.html");
+    let cityName = document.getElementById("cityName").innerText;
+    let lat = document.getElementById("coords").getAttribute("lat");
+    let long = document.getElementById("coords").getAttribute("long");
+    let position = {lat, long};
+
+    new google.maps.Marker({
+      position: position,
+      map,
+      title: cityName,
+      icon: image,
+      }).addListener("click", () => {
+        map.setCenter(position);
+        map.setZoom(6);
+        let pop = document.getElementById("cityInfo");
+        pop.style.display ='block';
+    });
+
   });
 
-  new google.maps.Marker({
-  position: position2,
-  map,
-  title: "City Name 2",
-  icon: image,
-  }).addListener("click", () => {
-	map.setCenter(position2);
-	map.setZoom(6);
-	$("#cityInfo").load("./cityData/test2.html");
-	let pop = document.getElementById("cityInfo");
-	pop.style.display ='block';
-  });
+
 
 
 }
