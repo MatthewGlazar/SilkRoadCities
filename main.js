4e071fe5b34da256cf828b9057db08a6483c7be5
Matthew Glazar <MatthewGlazar01@gmail.com>
2023-04-07 16:45:54 -0400

    Marker Automation

--- main.js
+++ main.js
@@ -2,19 +2,13 @@ import json from './cityData/cities.json' assert { type: 'json' };
 let map;
 
 async function initMap() {
-// The location of Uluru
-  //const position = { lat: 41.013889, lng: 28.955556 };
-  //const position2 = { lat: 39.654722, lng: 66.975833 };
-  // Request needed libraries.
-  //@ts-ignore
 
   const { Map } = await google.maps.importLibrary("maps");
-  //const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
+  //The Map
   map = new Map(document.getElementById("map"), {
   zoom: 3,
-  center: {lat: 39.533599785886885, lng: 83.49064664524137},
+  center: {lat: 39.533599785886885, lng: 83.49064664524137}, //Eyeballed Center
   mapId: "DEMO_MAP_ID",
   });
 
@@ -29,7 +23,6 @@ async function initMap() {
 
 
   let cities = json["cities"];
-  console.log(Object.keys(cities));
 
   Object.keys(cities).forEach(cityName => {
     let city = cities[cityName];
@@ -59,31 +52,8 @@ async function initMap() {
     }
   });
 
-  div.addEventListener('change', ()=> {
-    console.log("City Info Loaded");
-    let cityName = document.getElementById("cityName").innerText;
-    let lat = document.getElementById("coords").getAttribute("lat");
-    let long = document.getElementById("coords").getAttribute("long");
-    let position = {lat: lat, lng: long};
-
-    new google.maps.Marker({
-        position: position,
-        map,
-        title: cityName,
-        icon: image,
-        }).addListener("click", () => {
-        $("#cityInfo").load("./cityData/test.html");
-        map.setCenter(position);
-        map.setZoom(6);
-        let pop = document.getElementById("cityInfo");
-        pop.style.display ='block';
-    });
-  });
 
   mapContainer.appendChild(div);
-
-
-  //console.log(cities);
 
 }
 
